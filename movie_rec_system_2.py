3d64e5984b5388f3913860b464439c29eaaa655e
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2021-03-13 10:58:22 +0530

    Add files via upload

--- movie_rec_system_2.py
+++ movie_rec_system_2.py
@@ -13,9 +13,36 @@ dataset = {
 }
 
 user_1 = {'matrix','kgf','dabang','dhamaal','uri','bala','thor'}
-user_2 = {'kgf','it','uri','thor','dabang','raw','lucy','bala'}
+user_2 = {'kgf','it','uri','thor','dabang','raw','lucy'}
 scores = {}
 
 # 1. find out that category whose movies are most common between two users
 # 2. recommend those movies which user_1 has not seen yet but user_2 has seen and
 # those movies must belong to the category that we found in step 1
+
+common_movies = user_1.intersection(user_2)
+print(common_movies)
+scores = {}
+
+for key in dataset:
+    movies = dataset[key]
+    movies = set(movies)
+    numer = len(common_movies.intersection(movies))
+    denom = len(common_movies.union(movies))
+    sim = numer / denom
+    scores[key] = round(sim,2)
+
+print(scores)
+
+'''
+1. Find key whose value is max
+2. Find all movies of that key only
+3. Now show only those movies which user has not seen yet
+'''
+
+category = max(scores, key=scores.get)
+print("User has watched {} movies max".format(category))
+
+rec_movies = user_2.intersection(dataset[category]) - user_1
+print("Recommeded movies :",rec_movies)
+
