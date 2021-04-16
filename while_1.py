2ee48f9f4f79c9b0c336cbde2abf28b48ecb8bdf
Ravikant Tyagi <dhruvasharma1989@gmail.com>
2021-04-16 17:17:30 +0530

    Add files via upload

---
+++ while_1.py
@@ -0,0 +1,27 @@
+a = 0
+
+'''
+while a < 10:
+    print(a)
+    a = a + 1
+'''
+
+'''
+while True:
+    print(a)
+    a = a + 1
+    if a == 10:
+        break
+'''
+
+flag = True
+while flag:
+    print(a)
+    a = a + 1
+    if a == 10:
+        flag = False
+
+
+
+
+
